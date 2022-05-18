8ec7eacaf4858d771b0fb1f38e398c2b97967e47
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-05-19 02:03:49 +0500

    app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  getUsers() {
+    return [{ id: 1, name: 'Ulbi TV' }];
   }
 }
